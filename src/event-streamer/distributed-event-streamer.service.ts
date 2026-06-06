99520506f3d5ee904627b69cf052c1f379dbeb17
Divyanshu Motivaras <=>
2026-06-06 03:14:16 -0700

    following observable namming convention

--- src/event-streamer/distributed-event-streamer.service.ts
+++ src/event-streamer/distributed-event-streamer.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { BaseEventStreamer } from './event-stremer.abstract.service';
 import { RedisService } from '../redis/redis.service';
-import { Observable, Subject } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable()
 export class DistributedEventStreamer implements BaseEventStreamer {
-  constructor(private redisService: RedisService) {}
+  constructor(private redisService: RedisService) { }
 
   async getStream(id: string): Promise<Observable<string>> {
-    const subject = new Subject<string>();
+    const subject$ = new ReplaySubject<string>(2);
     await this.redisService.subscribe(id, (message: string) => {
-      subject.next(message);
+      subject$.next(message);
     });
 
-    return subject.asObservable();
+    return subject$.asObservable();
   }
 
   async sendEvent(id: string, message: string): Promise<void> {
